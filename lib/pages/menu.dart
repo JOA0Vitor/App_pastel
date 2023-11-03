2f0eb6800dc7d23c3b5b3256545c4d277d12f115
JOA0Vitor <vitorsilvasantana17@outlook.com>
2023-11-03 16:08:07 -0300

    menu

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -20,173 +20,235 @@ class _MyMenuState extends State<MyMenu> {
           title: const Text("Menu"),
           backgroundColor: const Color.fromARGB(255, 2, 133, 255),
         ),
-        body: const Column(
+        body: Column(
           children: [
-            SizedBox(
-              height: 10,
-            ),
             Padding(
-                padding: EdgeInsets.symmetric(vertical: 300, horizontal: 30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Quantidade", style: TextStyle(fontSize: 25)),
-                    Divider(
-                      height: 20,
-                      color: Colors.grey,
-                      thickness: 1,
-                      endIndent: 70,
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+              child: Column(
+                children: [
+                  const Row(
+                    children: [
+                      Text(
+                        "Seus dados",
+                        style: TextStyle(fontSize: 25),
+                        // textAlign: TextAlign.right,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    // color: Color.fromARGB(255, 180, 118, 118),
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 254, 254, 254),
+                      border: Border.all(
+                        width: 1.3,
+                      ),
+                      borderRadius: BorderRadius.circular(5),
                     ),
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Center(),
-                          ],
-                        ),
-                      ],
+                    child: const Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Total de Itens',
+                                style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 8),
+                                child: Text(
+                                  '14',
+                                  style: TextStyle(
+                                    color: Colors.black87,
+                                    fontSize: 21,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Quantidade vendida',
+                                style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 8),
+                                child: Text(
+                                  '54', //valores do bando de dados
+                                  style: TextStyle(
+                                    color: Colors.black87,
+                                    fontSize: 21,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ) //     ))),
-                ),
+                  ),
+
+                  // Divider(
+                  //   //linha pos nome
+                  //   height: 20,
+                  //   color: Colors.grey,
+                  //   thickness: 1,
+                  //   endIndent: 70,
+                  // ),
+                ],
+              ), //     ))),
+            ),
           ],
         ),
-        drawer: Drawer(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 40),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                InkWell(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: const Text(
-                      "Cadastro de itens",
-                      style: TextStyle(fontSize: 21),
-                    ),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context); //remover a aba
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const adicionarItem()),
-                    );
-                  },
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                InkWell(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: const Text(
-                      "Pesquisar",
-                      style: TextStyle(fontSize: 21),
-                    ),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context); //remover a aba
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const Pesquisar()),
-                    );
-                  },
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                InkWell(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: const Text(
-                      "Editar",
-                      style: TextStyle(fontSize: 21),
-                    ),
-                  ),
-                  onTap: () {},
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                InkWell(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: const Text(
-                      "Gerar Relatórios",
-                      style: TextStyle(fontSize: 21),
-                    ),
-                  ),
-                  onTap: () {},
-                ),
-                const SizedBox(
-                  height: 300,
-                ),
-                const Divider(),
-                InkWell(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child:
-                        // TextButton(
-                        //   onPressed: () {
-                        //      Navigator.pushReplacement(
-                        //             context,
-                        //             MaterialPageRoute(
-                        //                 builder: (context) => const MyMenu()));
-                        //   },
-                        // )
-                        const Text(
-                      "Sair",
-                      style: TextStyle(fontSize: 21),
-                    ),
-                  ),
-                  onTap: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext bc) {
-                          return AlertDialog(
-                            elevation: 8,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            title: const Text(
-                              'Sair',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            content: const Wrap(
-                              children: [
-                                Text('Você sairá do aplicativo!'),
-                                SizedBox(
-                                  height: 30,
-                                ),
-                                Text('Deseja realmente sair do aplicativo ?'),
-                              ],
-                            ),
-                            actions: [
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('Não')),
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const LoginPage()),
-                                    );
-                                  },
-                                  child: const Text('Sim'))
-                            ],
-                          );
-                        });
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
+        // drawer: Drawer(
+        //   child: Padding(
+        //     padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 40),
+        //     child: Column(
+        //       crossAxisAlignment: CrossAxisAlignment.start,
+        //       children: [
+        //         InkWell(
+        //           child: Container(
+        //             padding: const EdgeInsets.symmetric(vertical: 10),
+        //             child: const Text(
+        //               "Cadastro de itens",
+        //               style: TextStyle(fontSize: 21),
+        //             ),
+        //           ),
+        //           onTap: () {
+        //             Navigator.pop(context); //remover a aba
+        //             Navigator.push(
+        //               context,
+        //               MaterialPageRoute(
+        //                   builder: (context) => const adicionarItem()),
+        //             );
+        //           },
+        //         ),
+        //         const SizedBox(
+        //           height: 15,
+        //         ),
+        //         InkWell(
+        //           child: Container(
+        //             padding: const EdgeInsets.symmetric(vertical: 10),
+        //             child: const Text(
+        //               "Pesquisar",
+        //               style: TextStyle(fontSize: 21),
+        //             ),
+        //           ),
+        //           onTap: () {
+        //             Navigator.pop(context); //remover a aba
+        //             Navigator.push(
+        //               context,
+        //               MaterialPageRoute(
+        //                   builder: (context) => const Pesquisar()),
+        //             );
+        //           },
+        //         ),
+        //         const SizedBox(
+        //           height: 15,
+        //         ),
+        //         InkWell(
+        //           child: Container(
+        //             padding: const EdgeInsets.symmetric(vertical: 10),
+        //             child: const Text(
+        //               "Editar",
+        //               style: TextStyle(fontSize: 21),
+        //             ),
+        //           ),
+        //           onTap: () {},
+        //         ),
+        //         const SizedBox(
+        //           height: 15,
+        //         ),
+        //         InkWell(
+        //           child: Container(
+        //             padding: const EdgeInsets.symmetric(vertical: 10),
+        //             child: const Text(
+        //               "Gerar Relatórios",
+        //               style: TextStyle(fontSize: 21),
+        //             ),
+        //           ),
+        //           onTap: () {},
+        //         ),
+        //         const SizedBox(
+        //           height: 300,
+        //         ),
+        //         const Divider(),
+        //         InkWell(
+        //           child: Container(
+        //             padding: const EdgeInsets.symmetric(vertical: 10),
+        //             child:
+        //                 // TextButton(
+        //                 //   onPressed: () {
+        //                 //      Navigator.pushReplacement(
+        //                 //             context,
+        //                 //             MaterialPageRoute(
+        //                 //                 builder: (context) => const MyMenu()));
+        //                 //   },
+        //                 // )
+        //                 const Text(
+        //               "Sair",
+        //               style: TextStyle(fontSize: 21),
+        //             ),
+        //           ),
+        //           onTap: () {
+        //             showDialog(
+        //                 context: context,
+        //                 builder: (BuildContext bc) {
+        //                   return AlertDialog(
+        //                     elevation: 8,
+        //                     shape: RoundedRectangleBorder(
+        //                         borderRadius: BorderRadius.circular(10)),
+        //                     title: const Text(
+        //                       'Sair',
+        //                       style: TextStyle(fontWeight: FontWeight.bold),
+        //                     ),
+        //                     content: const Wrap(
+        //                       children: [
+        //                         Text('Você sairá do aplicativo!'),
+        //                         SizedBox(
+        //                           height: 30,
+        //                         ),
+        //                         Text('Deseja realmente sair do aplicativo ?'),
+        //                       ],
+        //                     ),
+        //                     actions: [
+        //                       TextButton(
+        //                           onPressed: () {
+        //                             Navigator.pop(context);
+        //                           },
+        //                           child: const Text('Não')),
+        //                       TextButton(
+        //                           onPressed: () {
+        //                             Navigator.pushReplacement(
+        //                               context,
+        //                               MaterialPageRoute(
+        //                                   builder: (context) =>
+        //                                       const LoginPage()),
+        //                             );
+        //                           },
+        //                           child: const Text('Sim'))
+        //                     ],
+        //                   );
+        //                 });
+        //           },
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
