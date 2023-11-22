3708274cd9b92a7aea015301c1db217d8292000b
JOA0Vitor <vitorsilvasantana17@outlook.com>
2023-11-22 03:36:23 -0300

    att adicionarItem

--- lib/pages/adicionarItem.dart
+++ lib/pages/adicionarItem.dart
@@ -1,73 +1,40 @@
-// ignore_for_file: file_names
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinbox/flutter_spinbox.dart';
-import 'package:trilhaapp/pages/menu.dart';
-import 'package:trilhaapp/services/adicionar_item_fire.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-const List<String> disponivelParaSelecionar = <String>[
-  'Bandeja Meia',
-  'Bandeja Completa'
-];
-
-String? dropdownValue;
-
-// ignore: camel_case_types
 class adicionarItem extends StatefulWidget {
-  const adicionarItem({super.key});
-
   @override
-  State<adicionarItem> createState() => _adicionarItemState();
+  adicionarItemState createState() => adicionarItemState();
 }
 
-class Item {
-  int id;
-  String nome;
-  int quantidade;
-  /*  bool disponivel; */
+class adicionarItemState extends State<adicionarItem> {
+  final TextEditingController _nomeProdutoController = TextEditingController();
+  int _quantidade = 0;
+  String _disponivel = "Bandeja Meia";
 
-  Item({
-    required this.id,
-    required this.nome,
-    required this.quantidade,
+  void _adicionarItem() async {
+    try {
+      // Obtenha uma referência para a coleção no Firebase
+      CollectionReference itemsCollection =
+          FirebaseFirestore.instance.collection('Itens adicionados');
 
-    /* required this.disponivel, */
-  });
-}
+      // Adicione os dados do item à coleção
+      await itemsCollection.add({
+        'disponivel': _disponivel,
+        'nomeProduto': _nomeProdutoController.text,
+        'quantidade': _quantidade,
+      });
 
-// ignore: camel_case_types
-class _adicionarItemState extends State<adicionarItem> {
-  final _nameController = TextEditingController();
-  double _idController = 0.0;
-  double _quantidadeController = 0.0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    dropdownValue;
-    _idController;
-    _nameController.text;
-  }
-
-  void _enviarParaFirestore() {
-    String name = _nameController.text;
-    List<String> tipoDeBandeja = disponivelParaSelecionar;
-    int quantidade = _quantidadeController.toInt();
-    int id = _idController.toInt();
-
-    // // Enviar para o Firestore usando as funções que mencionei anteriormente
-    adicionarDados(
-      id,
-      name,
-      tipoDeBandeja,
-      quantidade,
-    );
-
-    if (dropdownValue != null) {
-      print('Valor Selecionado: $dropdownValue');
-    } else {
-      print('Nenhum valor selecionado.');
+      // Mostrar uma mensagem de confirmação
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Item adicionado com sucesso!"),
+        ),
+      );
+    } catch (e) {
+      // Trate erros, se necessário
+      print('Erro ao adicionar item: $e');
     }
   }
 
@@ -76,274 +43,69 @@ class _adicionarItemState extends State<adicionarItem> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text("Adicionar itens"),
+          title: Text("Adicionar Itens"),
           backgroundColor: const Color.fromARGB(255, 2, 133, 255),
         ),
         body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 70, horizontal: 30),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  children: [
-                    const Text("ID do Item:", style: TextStyle(fontSize: 21)),
-                    Container(
-                      width: 166,
-                      margin: const EdgeInsets.only(
-                        left: 25,
-                      ),
-                      child: SpinBox(
-                        min: 1,
-                        max: 1000,
-                        value: _idController,
-
-                        // ignore: avoid_print
-                        onChanged: (value) {
-                          setState(() {
-                            _idController = value;
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          // border: InputBorder.none,
-                          contentPadding: EdgeInsets.zero,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
                 const SizedBox(
-                  height: 50,
+                  height: 30,
                 ),
                 const Text("Nome do produto:", style: TextStyle(fontSize: 21)),
-                TextField(
-                  controller: _nameController,
-                  style: const TextStyle(
-                      color: Colors.black87,
-                      fontSize: 21,
-                      decorationColor: Color.fromRGBO(255, 255, 255, 0.702)),
-                  // ignore: avoid_print
-                  onChanged: (value) => print(
-                      'O nome do produto é: $value (${value.characters.length})'),
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.only(top: 18),
-                    hintText: 'Digite nome do produto ',
-                    hintStyle: TextStyle(
-                        fontSize: 19), //Colocar as pavalras mais perto da linha
+                _buildTextField(
+                  controller: _nomeProdutoController,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(children: [
+                  const Text("Disponível:", style: TextStyle(fontSize: 21)),
+                  _buildDropdownButton(
+                    value: _disponivel,
+                    onChanged: (value) {
+                      setState(() {
+                        _disponivel = value!;
+                      });
+                    },
                   ),
-                ),
+                ]),
                 const SizedBox(
-                  height: 50,
+                  height: 30,
                 ),
-
-                Row(
-                  children: [
-                    const Text("Disponível:", style: TextStyle(fontSize: 21)),
-                    Container(
-                      width: 182,
-                      margin: const EdgeInsets.only(
-                        left: 10,
-                      ),
-                      child: Center(
-                        child: DropdownButton<String>(
-                          hint: const Center(child: Text('Selecione Um')),
-                          iconSize: 20,
-                          isExpanded: true,
-                          style: const TextStyle(
-                              fontSize: 19, color: Colors.black87),
-                          value: dropdownValue,
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              dropdownValue = newValue;
-                            });
-                          },
-                          items: disponivelParaSelecionar.map((String item) {
-                            return DropdownMenuItem<String>(
-                              value: item,
-                              child: Text(item),
-                            );
-                          }).toList(),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                const SizedBox(
-                  height: 50,
-                ),
-                // ignore: avoid_unnecessary_containers
                 Row(
                   children: [
                     const Text("Quantidade:", style: TextStyle(fontSize: 21)),
-                    Container(
-                      width: 150,
-                      margin: const EdgeInsets.only(
-                        left: 25,
-                      ),
-                      child: SpinBox(
-                        min: 1,
-                        max: 1000,
-                        value: _quantidadeController,
-
-                        // ignore: avoid_print
-                        onChanged: (value) {
-                          setState(() {
-                            _quantidadeController = value;
-                          });
-                        },
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                          // border: InputBorder.none,
-                          contentPadding: EdgeInsets.zero,
-                        ),
-                      ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    _buildSpinBox(
+                      labelText: "",
+                      onChanged: (value) {
+                        setState(() {
+                          _quantidade = value.toInt();
+                        });
+                      },
                     ),
                   ],
-                ), //ShowModalBottonSheet
-                const SizedBox(
-                  height: 40,
                 ),
-
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 50, horizontal: 50),
-                  child: TextButton(
-                    onPressed: () {
-                      _enviarParaFirestore();
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              scrollable: true,
-                              title: const Text(
-                                "Item adicionado!!",
-                                style: TextStyle(fontWeight: FontWeight.w600),
-                              ),
-                              elevation: 8,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                              content: Column(
-                                // direction: Axis.vertical,
-                                // mainAxisSize: MainAxisSize.max,
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  RichText(
-                                    text: TextSpan(children: <TextSpan>[
-                                      const TextSpan(
-                                        text: 'Id: ',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.black),
-                                      ),
-                                      TextSpan(
-                                          text:
-                                              _idController.toStringAsFixed(0),
-                                          style: const TextStyle(
-                                              color: Colors.blue,
-                                              fontSize: 18)),
-                                    ]),
-                                  ),
-                                  // Text(
-                                  //   " ${_idController.toStringAsFixed(0)}",
-                                  // ),
-                                  const SizedBox(
-                                    height: 30,
-                                  ),
-                                  RichText(
-                                    text: TextSpan(children: <TextSpan>[
-                                      const TextSpan(
-                                        text: '   Nome do produto:\n\n',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.black),
-                                      ),
-                                      TextSpan(
-                                          text: _nameController.text,
-                                          style: const TextStyle(
-                                              color: Colors.blue,
-                                              fontSize: 16)),
-                                    ]),
-                                  ),
-                                  // Text(
-                                  //     "Nome do produto:\n ${_nameController.text} "),
-                                  const SizedBox(
-                                    height: 30,
-                                  ),
-                                  RichText(
-                                    text: TextSpan(children: <TextSpan>[
-                                      const TextSpan(
-                                        text: 'Tipo de Bandeja:\n\n',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.black),
-                                      ),
-                                      TextSpan(
-                                          text: dropdownValue,
-                                          style: const TextStyle(
-                                              color: Colors.blue,
-                                              fontSize: 18)),
-                                    ]),
-                                  ),
-
-                                  const SizedBox(
-                                    height: 30,
-                                  ),
-                                  RichText(
-                                    text: TextSpan(children: <TextSpan>[
-                                      const TextSpan(
-                                        text: 'Quantidade: ',
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.black),
-                                      ),
-                                      TextSpan(
-                                          text: _quantidadeController
-                                              .toStringAsFixed(0),
-                                          style: const TextStyle(
-                                            color: Colors.blue,
-                                            fontSize: 18,
-                                          )),
-                                    ]),
-                                  ),
-                                ],
-                              ),
-                              actions: [
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  const adicionarItem()));
-                                    },
-                                    child: const Text("Adicionar novo item")),
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  const MyMenu()));
-                                    },
-                                    child: const Text("Voltar para Menu"))
-                              ],
-                            );
-                          });
-                    },
+                const SizedBox(
+                  height: 70,
+                ),
+                Center(
+                  child: ElevatedButton(
                     style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.blue),
-                      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                          const EdgeInsets.symmetric(
-                              horizontal: 50, vertical: 20)),
-                    ),
-                    child: const Text(
-                      'Adicionar',
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                        padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+                            horizontal: 50, vertical: 20))),
+                    onPressed: () {
+                      _adicionarItem();
+                    },
+                    child: Text(
+                      "Adicionar",
                       style: TextStyle(color: Colors.white, fontSize: 21),
                     ),
                   ),
@@ -352,6 +114,79 @@ class _adicionarItemState extends State<adicionarItem> {
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+  }) {
+    return TextField(
+        controller: controller,
+        style: const TextStyle(
+            color: Colors.black87,
+            fontSize: 21,
+            decorationColor: Color.fromRGBO(255, 255, 255, 0.702)),
+        decoration: const InputDecoration(
+          hintText: 'Digite nome do produto ',
+          hintStyle: TextStyle(fontSize: 17),
+          labelStyle: TextStyle(
+            fontSize: 21,
+            color: Colors.blue, // Cor do rótulo
+          ),
+        ));
+  }
+
+  Widget _buildSpinBox({
+    required String labelText,
+    required ValueChanged<double> onChanged,
+  }) {
+    return Row(
+      children: [
+        Container(
+          width: 150,
+          margin: const EdgeInsets.only(
+            left: 25,
+          ),
+          child: SpinBox(
+            min: 0,
+            max: 1000,
+            value: _quantidade.toDouble(),
+            onChanged: onChanged,
+            decoration: InputDecoration(
+              labelText: labelText,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDropdownButton({
+    required String value,
+    required ValueChanged<String?> onChanged,
+  }) {
+    return Container(
+      width: 182,
+      margin: const EdgeInsets.only(
+        left: 10,
+      ),
+      child: DropdownButton<String>(
+        iconSize: 30,
+        isExpanded: true,
+        style: const TextStyle(fontSize: 18, color: Colors.black87),
+        value: value,
+        items: const [
+          DropdownMenuItem(
+            value: "Bandeja Meia",
+            child: Text("Bandeja Meia"),
+          ),
+          DropdownMenuItem(
+            value: "Bandeja Completa",
+            child: Text("Bandeja Completa"),
+          ),
+        ],
+        onChanged: onChanged,
       ),
     );
   }
